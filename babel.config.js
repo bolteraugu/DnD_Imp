e80ed6ba013a5928bf09d5a3d82a1e9bb8dd1c62
August Bolter <bolteraugu@ecs.vuw.ac.nz>
2021-05-17 11:01:26 +0000

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-/** Fairly sure this is the default code for babel (JavaScript compiler) for expo*/
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"], //Extends default preset and adds support for font icons, decorators etc...
-  };
-};
